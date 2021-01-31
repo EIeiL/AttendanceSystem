a47f3287fbd65dc42c6a2e833158648d6ab10ca0
EIeiL <1257093341@qq.com>
2021-01-31 21:57:31 +0800

    考勤组模态框接口30%

--- src/views/attendanceManage/attendanceGroup/index.vue
+++ src/views/attendanceManage/attendanceGroup/index.vue
@@ -44,119 +44,122 @@ import ChoseUserModal from "./components/choseUser";
 
 // js文件 -- 1传入组件内容、2深拷贝、3弹框
 import { attendanceGroup } from "@/enum/attendance";
-import deepCopy from "@/utils/deepCopy"
-import { openDel, test } from "@/utils/messageBox"
+import deepCopy from "@/utils/deepCopy";
+import { openDel, test } from "@/utils/messageBox";
 
 export default {
   components: {
     Current, // 当前位置
     AttendanceForm, // 表单
     AttendanceTable, // 表格+分页
-    AddGroupModal,  // 新增+编辑模态框
-    ChoseUserModal  // 选择人员模态框
+    AddGroupModal, // 新增+编辑模态框
+    ChoseUserModal, // 选择人员模态框
   },
-  data () {
+  data() {
     return {
-      rowData: {},  // 某行数据
+      rowData: {}, // 某行数据
       dialogVisible: false, // 控制新增+编辑模态框是否显示
-      isChoseUser: false,  // 控制选择人员模态框是否显示
+      isChoseUser: false, // 控制选择人员模态框是否显示
       modalTitle: "", // 模态框标题
       formOptions: [], // 表单属性
       btnTools: attendanceGroup.btnTools, // 工具按钮属性
       columnOptions: attendanceGroup.columnOptions, // 列属性
       tableData: [], // 页面显示数据 -- 表格
-      searchParam: {  // 分页相关
+      searchParam: {
+        // 分页相关
         currPage: 1,
         pageSize: 10,
       },
       count: 0, // 总的数据条数
     };
   },
-  created () {
-    this.getGroupList(1)
+  created() {
+    this.getGroupList(1);
   },
   methods: {
-    openDel,  // 声明弹框事件
+    openDel, // 声明弹框事件
     /**
      * @description 工具按钮返回内容
      */
-    toolsBtn (val) {
+    toolsBtn(val) {
       console.log(val);
       this.modalTitle = "添加考勤组";
+      this.rowData = {};
       this.dialogVisible = true;
     },
     /**
      * @description 新增+编辑模态框返回内容
      */
-    onModal (val) {
+    onModal(val) {
       console.log(val);
     },
     /**
      * @description 选择人员模态框返回内容
      */
-    onModal1 (val) {
+    onModal1(val) {
       console.log(val);
+      console.log("rowData", this.rowData);
     },
     /**
      * @description 点击删除触发事件
      */
-    async onTableDel (val) {
+    async onTableDel(val) {
       // console.log(val);
       // this.openDel('是否确定删除该考勤组？')
       var res = await this.$request.delGroup({
-        id: val.id
-      })
+        id: val.id,
+      });
       if (res.code == 0) {
-        console.log('删除success')
-        this.getGroupList(1)
+        console.log("删除success");
+        this.getGroupList(1);
       }
     },
     /**
      * @description 点击编辑触发事件
      */
-    onTableEdit (val) {
-      console.log('编辑', val);
+    onTableEdit(val) {
+      console.log("编辑", val);
       // this.rowData = JSON.parse(val)
       var o;
-      if (typeof val === 'object') {
+      if (typeof val === "object") {
         o = deepCopy.copyObject(val);
       } else {
         o = val;
       }
-      this.rowData = o
+      this.rowData = o;
       // console.log(this.rowData);
-      this.modalTitle = "编辑考勤组"
+      this.modalTitle = "编辑考勤组";
       this.dialogVisible = true;
     },
     /**
      * @description 选择每页数据条数返回内容
      */
-    pageSize (val) {
-      this.searchParam.pageSize = val
-      const currPage = parseInt(this.count / this.searchParam.pageSize) + 1
-      this.getGroupList(currPage)
+    pageSize(val) {
+      this.searchParam.pageSize = val;
+      const currPage = parseInt(this.count / this.searchParam.pageSize) + 1;
+      this.getGroupList(currPage);
     },
     /**
      * @description 跳转至某一页选择内容
      */
-    currPage (val) {
-      this.searchParam.currPage = val
-      this.getGroupList(this.searchParam.currPage)
+    currPage(val) {
+      this.searchParam.currPage = val;
+      this.getGroupList(this.searchParam.currPage);
     },
     /**
      * @description 获取考勤组数据
      */
-    async getGroupList (page) {
-      this.searchParam.currPage = page
-      console.log('123');
+    async getGroupList(page) {
+      this.searchParam.currPage = page;
+      console.log("123");
       const res = await this.$request.getGroup({
-        ...this.searchParam
-      })
-      console.log('res', res)
+        ...this.searchParam,
+      });
+      console.log("res", res);
       if (res.code == 0) {
         // this.loading = false
-        this.count = res.data.total
-        this.tableData = res.data.list
+        this.count = res.data.total;
+        this.tableData = res.data.list;
       }
     },
   },
